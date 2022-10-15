31d3c0b62ed24864f46acde04edd0975f225336f
Adam Czubernat <czubernat@gmail.com>
2022-10-15 15:44:41 -0400

    Add navigation from signup activity to home page for either driver or company to respected pages.

--- app/src/main/java/sheridan/czuberad/sideeye/SignupActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/SignupActivity.kt
@@ -1,18 +1,36 @@
 package sheridan.czuberad.sideeye
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 
 class SignupActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        var isCompany = false
+        var toggle:Switch = findViewById(R.id.signup_switch)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            isCompany = isChecked
+        }
+
         val homeClick = findViewById<Button>(R.id.signupHome_button)
         homeClick.setOnClickListener{
-            val intent = Intent(this, HomeDriverActivity::class.java)
-            startActivity(intent)
+
+            if (isCompany){
+                val intent = Intent(this, HomeCompanyActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                val intent = Intent(this, HomeDriverActivity::class.java)
+                startActivity(intent)
+            }
+
         }
     }
 }
