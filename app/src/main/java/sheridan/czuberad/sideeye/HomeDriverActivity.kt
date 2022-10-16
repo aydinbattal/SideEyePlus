b209b488ad9ef34ea826057ec8cccd2e5c5030a4
Adam Czubernat <czubernat@gmail.com>
2022-10-15 21:38:25 -0400

    Implement navigation from driver home page to tracking and reaction test page.

--- app/src/main/java/sheridan/czuberad/sideeye/HomeDriverActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/HomeDriverActivity.kt
@@ -1,11 +1,25 @@
 package sheridan.czuberad.sideeye
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeDriverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_driver)
+
+        val eyeClick = findViewById<Button>(R.id.button_eye)
+        eyeClick.setOnClickListener {
+            val intent = Intent(this, EyeDetectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        val testClick = findViewById<Button>(R.id.button_test)
+        testClick.setOnClickListener {
+            val intent = Intent(this, ReactionTestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
