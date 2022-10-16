a45ecb76f946843841b5b93e51ffb09bdf766491
Adam Czubernat <czubernat@gmail.com>
2022-10-15 22:49:07 -0400

    Implement UI for Reaction/Focus test page.

--- app/src/main/java/sheridan/czuberad/sideeye/ReactionTestActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/ReactionTestActivity.kt
@@ -7,5 +7,7 @@ class ReactionTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reaction_test)
+
+
     }
 }
