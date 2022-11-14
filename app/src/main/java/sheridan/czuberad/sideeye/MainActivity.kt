0f63859ddfbda812c960be3c59ac7100deec79e3
Adam Czubernat <czubernat@gmail.com>
2022-11-13 20:53:51 -0500

    Firebase Login: Implement login functionality for driver and company with firebase authentication.

--- app/src/main/java/sheridan/czuberad/sideeye/MainActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/MainActivity.kt
@@ -4,16 +4,16 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Switch
-import android.widget.ToggleButton
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
         var isCompany: Boolean = false
         var toggle:Switch = findViewById(R.id.switch_login)
         toggle.setOnCheckedChangeListener { _, isChecked ->
@@ -22,13 +22,31 @@ class MainActivity : AppCompatActivity() {
 
         val loginClick = findViewById<Button>(R.id.button_login)
         loginClick.setOnClickListener{
+            var emailText = findViewById<EditText>(R.id.text_email_login).text.toString().trim()
+            var passwordText = findViewById<EditText>(R.id.text_password_login).text.toString().trim()
             if (isCompany){
-                val intent = Intent(this, HomeCompanyActivity::class.java)
-                startActivity(intent)
+                auth.signInWithEmailAndPassword(emailText,passwordText).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, HomeCompanyActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(baseContext, "UNABLE TO LOGIN - COMPANY", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             }
             else{
-                val intent = Intent(this, HomeDriverActivity::class.java)
-                startActivity(intent)
+                auth.signInWithEmailAndPassword(emailText, passwordText).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, HomeDriverActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(baseContext, "UNABLE TO LOGIN - DRIVER", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             }
 
         }
