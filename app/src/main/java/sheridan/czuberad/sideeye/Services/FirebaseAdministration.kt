95e754c23ab89c3ad86628c9ca409bc05f92f8f6
Adam Czubernat <czubernat@gmail.com>
2022-11-13 20:38:07 -0500

    Firebase Signup: implement firebase authentication for singing up either driver or company.

--- app/src/main/java/sheridan/czuberad/sideeye/Services/FirebaseAdministration.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Services/FirebaseAdministration.kt
@@ -1,4 +1,31 @@
 package sheridan.czuberad.sideeye.Services
 
+import com.google.firebase.auth.FirebaseAuth
+
 class FirebaseAdministration {
+
+    var sign: Boolean = false
+    fun signupDriver(email: String, password: String, auth: FirebaseAuth){
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+            if(it.isComplete){
+                if(it.isSuccessful){
+                    signupSuccess()
+                }
+                else{
+                    signupFail()
+                }
+            }
+
+
+        }
+
+    }
+
+    private fun signupSuccess() {
+        sign = true
+    }
+    private fun signupFail(){
+        sign = false
+    }
 }
--- app/src/main/java/sheridan/czuberad/sideeye/SignupActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/SignupActivity.kt
@@ -7,13 +7,19 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import sheridan.czuberad.sideeye.Services.FirebaseAdministration
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private var admin = FirebaseAdministration()
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
@@ -24,21 +30,46 @@ class SignupActivity : AppCompatActivity() {
             isCompany = isChecked
         }
 
-        var email_text = findViewById<EditText>(R.id.text_email_signup)
-        var password_text = findViewById<EditText>(R.id.text_password_signup)
+
 
 
         val homeClick = findViewById<Button>(R.id.button_signup)
         homeClick.setOnClickListener{
+            var emailText = findViewById<EditText>(R.id.text_email_signup).text.toString().trim()
+            var passwordText = findViewById<EditText>(R.id.text_password_signup).text.toString().trim()
 
             if (isCompany){
-                val intent = Intent(this, HomeCompanyActivity::class.java)
-                startActivity(intent)
+                auth.createUserWithEmailAndPassword(emailText, passwordText).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, HomeCompanyActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(baseContext, "UNABLE TO SIGNUP - COMPANY",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
             }
 
             else{
-                val intent = Intent(this, HomeDriverActivity::class.java)
-                startActivity(intent)
+                auth.createUserWithEmailAndPassword(emailText, passwordText).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, HomeDriverActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(baseContext, "UNABLE TO SIGNUP - COMPANY",Toast.LENGTH_SHORT).show()
+                    }
+                }
+//                admin.signupDriver(emailText, passwordText, auth)
+//                if(admin.sign){
+//                    val intent = Intent(this, HomeDriverActivity::class.java)
+//                    startActivity(intent)
+//                }
+//                else{
+//                    Toast.makeText(baseContext, emailText + passwordText,Toast.LENGTH_SHORT).show()
+//                }
+
             }
 
         }
