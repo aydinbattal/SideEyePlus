4cbb674d5981dc4d0e03c9a95d47173fb0ed814e
Adam Czubernat <czubernat@gmail.com>
2022-11-17 18:56:24 -0500

    Firestore/Driver Eye Detection: refactor code, create functions within application logic and driver service class for interaction with application class.

--- app/src/main/java/sheridan/czuberad/sideeye/EyeDetectionActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/EyeDetectionActivity.kt
@@ -9,10 +9,9 @@ import android.widget.Toast
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import sheridan.czuberad.sideeye.Domain.Alert
 import sheridan.czuberad.sideeye.Domain.Session
-import java.sql.Timestamp
-import java.util.Date
+import sheridan.czuberad.sideeye.Services.DriverService
+import sheridan.czuberad.sideeye.`Application Logic`.EyeDetectionLogic
 import java.util.UUID
 
 class EyeDetectionActivity : AppCompatActivity() {
@@ -21,43 +20,25 @@ class EyeDetectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eye_detection)
-
-        val textView = findViewById<TextView>(R.id.textView_eye_detection)
-        //textView.text = "yooooooooo"
+        var eyeDetectionLogic = EyeDetectionLogic()
+        var driverService = DriverService()
 
         val detectOnclick = findViewById<Button>(R.id.button_eye_detection)
 
         detectOnclick.setOnClickListener {
 
-            val timestamp = Timestamp(System.currentTimeMillis())
-            val dateStart = Date(timestamp.time)
+            //val timestamp = Timestamp(System.currentTimeMillis())
+            val dateStart = eyeDetectionLogic.getTimeStamp()
             Toast.makeText(baseContext, dateStart.toString(), Toast.LENGTH_SHORT).show()
 
             val endSessionOnClick = findViewById<Button>(R.id.button_eye_detection_end)
             endSessionOnClick.setOnClickListener {
-                val timestamp1 = Timestamp(System.currentTimeMillis())
-                val dateEnd = Date(timestamp1.time)
+
+                val dateEnd = eyeDetectionLogic.getTimeStamp()
                 Toast.makeText(baseContext, dateEnd.toString(), Toast.LENGTH_SHORT).show()
 
-                var session = Session(dateStart, dateEnd)
-                val currentUser = Firebase.auth.currentUser
-                db = FirebaseFirestore.getInstance()
-                val uid = UUID.randomUUID().toString()
-
-                var AlertList = arrayListOf<Alert>()
-                AlertList.add(Alert("Eye"))
-                AlertList.add(Alert("Face"))
-                if (currentUser != null) {
-                    db.collection("Drivers").document(currentUser.uid).collection("Sessions").document(uid).set(session).addOnCompleteListener {
-                        if (it.isSuccessful){
-                            AlertList.forEach{ alert ->
-                                db.collection("Drivers").document(currentUser.uid).collection("Sessions").document(uid).collection("Alerts").document().set(alert)
-                            }
-                        }
-                    }
-                }
-
-
+                val session = Session(dateStart, dateEnd)
+                driverService.addAlertToSessionById(Firebase.auth.currentUser, FirebaseFirestore.getInstance(), UUID.randomUUID().toString(), session)
 
             }
 
--- app/src/main/java/sheridan/czuberad/sideeye/Services/DriverService.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Services/DriverService.kt
@@ -2,7 +2,9 @@ package sheridan.czuberad.sideeye.Services
 
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import sheridan.czuberad.sideeye.Domain.Alert
 import sheridan.czuberad.sideeye.Domain.Driver
+import sheridan.czuberad.sideeye.Domain.Session
 
 class DriverService {
 
@@ -14,5 +16,26 @@ class DriverService {
                    var driverData = document.toObject(Driver::class.java) ?: Driver()
                 }
         }
+    }
+
+    fun addAlertToSessionById(
+        currentUser: FirebaseUser?,
+        db: FirebaseFirestore,
+        uid: String,
+        session: Session
+    ) {
+        var AlertList = arrayListOf<Alert>()
+        AlertList.add(Alert("Eye"))
+        AlertList.add(Alert("Face"))
+        if (currentUser != null) {
+            db.collection("Drivers").document(currentUser.uid).collection("Sessions").document(uid).set(session).addOnCompleteListener {
+                if (it.isSuccessful){
+                    AlertList.forEach{ alert ->
+                        db.collection("Drivers").document(currentUser.uid).collection("Sessions").document(uid).collection("Alerts").document().set(alert)
+                    }
+                }
+            }
+        }
+
     }
 }
