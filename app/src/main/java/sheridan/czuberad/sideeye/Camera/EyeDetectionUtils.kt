b7cf607b3f9f813949355f725b39e6f1691455eb
Adam Czubernat <czubernat@gmail.com>
2022-12-09 22:38:28 -0500

    EyeDetectionUtils, ImageAnalyzer: Implement image analysis with mlkit, update layout to display camera preview.

--- app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
@@ -1,5 +1,29 @@
 package sheridan.czuberad.sideeye.Camera
+import android.content.ContentValues.TAG
+import android.nfc.Tag
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class EyeDetectionUtils :ImageAnalyzer<List<Face>>() {
+
+    private val realTimeOpts = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+        .build()
+
+    private val det = FaceDetection.getClient(realTimeOpts)
+
+    override fun detectFace(image: InputImage): Task<List<Face>> {
+        return det.process(image)
+    }
+
+    override fun onSuccess(results: List<Face>){
+        results.forEach {
+            Log.d(TAG,it.rightEyeOpenProbability.toString())
+        }
+    }
 }
--- app/src/main/java/sheridan/czuberad/sideeye/Camera/ImageAnalyzer.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Camera/ImageAnalyzer.kt
@@ -1,12 +1,26 @@
 package sheridan.czuberad.sideeye.Camera
 
+import android.annotation.SuppressLint
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
 
 abstract class ImageAnalyzer<T> : ImageAnalysis.Analyzer {
 
-
+    @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
     override fun analyze(image: ImageProxy) {
+        val imageM = image.image
+        imageM?.let {
+
+        }
 
     }
+
+    protected abstract fun detectFace(image: InputImage): Task<T>
+
+    protected abstract fun onSuccess(
+        results: T
+    )
+
 }
