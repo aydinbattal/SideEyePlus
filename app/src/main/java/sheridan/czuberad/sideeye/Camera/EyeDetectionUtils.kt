871760b680bae34698f575a1930ad2e0f2057fd8
Adam Czubernat <czubernat@gmail.com>
2022-12-09 20:41:41 -0500

    CameraX: Update camera preview function, add configuration for camera to use from facing camera, initialize eye detection processing class for image Analysis.

--- app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
@@ -1,4 +1,5 @@
 package sheridan.czuberad.sideeye.Camera
+import com.google.mlkit.vision.face.Face
 
-class EyeDetectionUtils {
+class EyeDetectionUtils :ImageAnalyzer<List<Face>>() {
 }
--- app/src/main/java/sheridan/czuberad/sideeye/Camera/ImageAnalyzer.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Camera/ImageAnalyzer.kt
@@ -1,4 +1,12 @@
 package sheridan.czuberad.sideeye.Camera
 
-class ImageAnalyzer {
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+abstract class ImageAnalyzer<T> : ImageAnalysis.Analyzer {
+
+
+    override fun analyze(image: ImageProxy) {
+
+    }
 }
