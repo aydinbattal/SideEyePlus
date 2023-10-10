8199c69b2fa02e31ce91711deea7f313666d3ab3
Adam Czubernat <czubernatadam@gmail.com>
2023-10-10 19:03:48 -0400

    Convert DriverHome page to use Jetpack Compose UI, replace UI components from XML for driver home page to use Compose componements. Design GraphView to be used to display Session History.

--- app/src/main/java/sheridan/czuberad/sideeye/HomeDriverActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/HomeDriverActivity.kt
@@ -6,6 +6,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseUser
@@ -15,6 +39,9 @@ import com.google.firebase.ktx.Firebase
 import sheridan.czuberad.sideeye.Domain.Driver
 import sheridan.czuberad.sideeye.HomeDriverActivity.Companion.REQUEST_CODE_PERMISSIONS
 import sheridan.czuberad.sideeye.HomeDriverActivity.Companion.REQUIRED_PERMISSIONS
+import sheridan.czuberad.sideeye.UI.DriverHome
+import kotlin.math.roundToInt
+
 
 class HomeDriverActivity : AppCompatActivity() {
     private lateinit var uid: String
@@ -22,30 +49,36 @@ class HomeDriverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_driver)
-        checkPermissions()
-        val currentUser = Firebase.auth.currentUser
-
-        val nameText = findViewById<TextView>(R.id.textView_driver_home_name)
-        db = FirebaseFirestore.getInstance()
-        getDriverData(currentUser, nameText)
-        //val driverService = DriverService()
-        //driverService.getDriverData(db, currentUser)
-
-
-        val eyeClick = findViewById<Button>(R.id.button_eye)
-        eyeClick.setOnClickListener {
-            val intent = Intent(this, EyeDetectionActivity::class.java)
-            startActivity(intent)
+        setContent {
+            DriverHome()
         }
-
-        val testClick = findViewById<Button>(R.id.button_test)
-        testClick.setOnClickListener {
-            val intent = Intent(this, ReactionTestActivity::class.java)
-            startActivity(intent)
-        }
+//        setContentView(R.layout.activity_home_driver)
+//        checkPermissions()
+//        val currentUser = Firebase.auth.currentUser
+//
+//        val nameText = findViewById<TextView>(R.id.textView_driver_home_name)
+//        db = FirebaseFirestore.getInstance()
+//        getDriverData(currentUser, nameText)
+//        //val driverService = DriverService()
+//        //driverService.getDriverData(db, currentUser)
+//
+//
+//        val eyeClick = findViewById<Button>(R.id.button_eye)
+//        eyeClick.setOnClickListener {
+//            val intent = Intent(this, EyeDetectionActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        val testClick = findViewById<Button>(R.id.button_test)
+//        testClick.setOnClickListener {
+//            val intent = Intent(this, ReactionTestActivity::class.java)
+//            startActivity(intent)
+//        }
 
     }
+
+
+
     private fun isPermissionsAllowed() = HomeDriverActivity.REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext,it) == PackageManager.PERMISSION_GRANTED
     }
---
+++ app/src/main/java/sheridan/czuberad/sideeye/UI/DriverHome.kt
@@ -0,0 +1,248 @@
+package sheridan.czuberad.sideeye.UI
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import sheridan.czuberad.sideeye.R
+import kotlin.math.roundToInt
+
+
+@Composable
+fun DriverHome() {
+    val data = listOf(
+        Pair(6, 111.45),
+        Pair(7, 111.0),
+        Pair(8, 113.45),
+        Pair(9, 112.25),
+        Pair(10, 116.45),
+        Pair(11, 113.35),
+        Pair(12, 118.65),
+        Pair(13, 110.15),
+        Pair(14, 113.05),
+        Pair(15, 114.25),
+        Pair(16, 116.35),
+        Pair(17, 117.45),
+        Pair(18, 112.65),
+        Pair(19, 115.45),
+        Pair(20, 111.85)
+    )
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        InfoCard()
+
+        Card(
+            modifier = Modifier
+                .width(screenWidth)
+                .height(screenHeight / 3)
+                .zIndex(1f)
+                .offset(y = (-50).dp)
+                .padding(10.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            )// Optional: if you want to remove rounded corners
+            // Optional: if you want to set a custom color
+        ) {
+            LineChart(
+                data = data,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+
+        }
+
+        
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(3.dp),
+            horizontalArrangement = Arrangement.Center) {
+            Button(
+                modifier = Modifier.weight(1f),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF39AFEA)),
+                onClick = { /* TODO: Handle button click */ }) {
+                Text("Session Tracking")
+            }
+            Spacer(modifier = Modifier.width(5.dp))
+            Button(
+
+                shape = RectangleShape,
+                modifier = Modifier.weight(1f),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF39AFEA)),
+                onClick = { /* TODO: Handle button click */ }) {
+                Text("Reaction Test")
+            }
+            
+        }
+
+//        LineChart(
+//            data = data,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(300.dp)
+//                .align(Alignment.CenterHorizontally)
+//                .border(1.dp, Color.Blue)
+//        )
+    }
+
+
+}
+
+@Composable
+fun LineChart(
+    data: List<Pair<Int, Double>> = emptyList(),
+    modifier: Modifier = Modifier,
+    title: String = "Session Overview"
+) {
+    val spacing = 100f
+    val graphColor = Color(0xFF39AFEA)
+    val upperValue = remember { (data.maxOfOrNull { it.second }?.plus(1))?.roundToInt() ?: 0 }
+    val lowerValue = remember { (data.minOfOrNull { it.second }?.toInt() ?: 0) }
+
+    val drawingProgress = remember { Animatable(0f) }
+
+    LaunchedEffect(drawingProgress) {
+        drawingProgress.animateTo(
+            targetValue = data.size.toFloat(),
+            animationSpec = tween(durationMillis = 2000)
+        )
+    }
+
+
+
+    Canvas(modifier = modifier) {
+        val spacePerHour = (size.width - spacing) / data.size
+        val height = size.height
+
+        val path = Path()
+        val drawingLimit = drawingProgress.value.toInt()
+
+        for (i in 0 until drawingLimit) {
+            val info = data[i]
+            val ratio = (info.second - lowerValue) / (upperValue - lowerValue)
+            val x1 = spacing + i * spacePerHour
+            val y1 = height - spacing - (ratio * height).toFloat()
+
+            if (i == 0) {
+                path.moveTo(x1, y1)
+            } else {
+                path.lineTo(x1, y1)
+            }
+        }
+
+        if (drawingLimit > 0 && drawingLimit < data.size) {
+            val startInfo = data[drawingLimit - 1]
+            val endInfo = data[drawingLimit]
+            val t = drawingProgress.value - drawingLimit
+
+            val startX = spacing + (drawingLimit - 1) * spacePerHour
+            val startY = height - spacing - ((startInfo.second - lowerValue) / (upperValue - lowerValue) * height).toFloat()
+
+            val endX = spacing + drawingLimit * spacePerHour
+            val endY = height - spacing - ((endInfo.second - lowerValue) / (upperValue - lowerValue) * height).toFloat()
+
+            val interpolatedX = lerp(startX, endX, t)
+            val interpolatedY = lerp(startY, endY, t)
+
+            path.lineTo(interpolatedX, interpolatedY)
+        }
+
+        drawPath(
+            path = path,
+            color = graphColor,
+            style = Stroke(
+                width = 2.dp.toPx(),
+                cap = StrokeCap.Round
+            )
+        )
+
+    }
+}
+
+@Composable
+fun InfoCard(){
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenHeight = configuration.screenHeightDp.dp
+
+    Card(modifier = Modifier
+        .width(screenWidth)
+        .height(screenHeight / 3),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+    colors = CardDefaults.cardColors(
+        containerColor = Color(0xFF39AFEA)
+    ),
+    shape = RectangleShape) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+
+            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "profile pic")
+            Column {
+                Text(
+                    text = "JOHN DOE",
+                    color = Color.White
+                )
+                Text(
+                    text = "Independent Driver",
+                    color = Color.White
+                )
+
+            }
+
+        }
+
+    }
+}
+
+fun lerp(start: Float, stop: Float, fraction: Float): Float {
+    return start + fraction * (stop - start)
+}
