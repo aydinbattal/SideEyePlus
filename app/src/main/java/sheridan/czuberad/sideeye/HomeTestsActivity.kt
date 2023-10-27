7ae0a42c9171bdb7fe7c1b3befb5304405ae562d
Aydin Battal <abattal35@gmail.com>
2023-10-27 15:09:28 -0400

    Implemented second reaction test (questionnaire) with placeholder questions

--- app/src/main/java/sheridan/czuberad/sideeye/HomeTestsActivity.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/HomeTestsActivity.kt
@@ -11,9 +11,12 @@ import sheridan.czuberad.sideeye.databinding.ActivityHomeTestsBinding
 
 class HomeTestsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeTestsBinding
-    private val requestCode = 1 // Define a request code
+    private val reactionTestRequestCode = 1
+    private val questionnaireRequestCode = 2
     private lateinit var startButton1: Button
+    private lateinit var startButton2: Button
     private lateinit var reactionTestResult1: TextView
+    private lateinit var reactionTestResult2: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,18 @@ class HomeTestsActivity : AppCompatActivity() {
         setContentView(view)
 
         startButton1 = binding.reactionTest1Button
+        startButton2 = binding.reactionTest2Button
         reactionTestResult1 = binding.reactionResult1TextView
+        reactionTestResult2 = binding.reactionResult2TextView
 
         startButton1.setOnClickListener {
             val intent = Intent(this@HomeTestsActivity, ReactionTestActivity::class.java)
-            startActivityForResult(intent, requestCode) // Start the activity for a result
+            startActivityForResult(intent, reactionTestRequestCode) // Start the activity for a result
+        }
+
+        startButton2.setOnClickListener {
+            val intent = Intent(this@HomeTestsActivity, QuestionnaireActivity::class.java)
+            startActivityForResult(intent, questionnaireRequestCode)
         }
     }
 
@@ -35,7 +45,7 @@ class HomeTestsActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == this.requestCode && resultCode == Activity.RESULT_OK) {
+        if (requestCode == reactionTestRequestCode && resultCode == Activity.RESULT_OK) {
             val completedTest = data?.getBooleanExtra("completedTest", false) ?: false
             val averageReactionTime = data?.getLongExtra("averageReactionTime", 0L) ?: 0L
 
@@ -47,6 +57,18 @@ class HomeTestsActivity : AppCompatActivity() {
 
             }
 
+        } else if (requestCode == questionnaireRequestCode && resultCode == Activity.RESULT_OK) {
+            // Handle result from QuestionnaireActivity
+            val completedTest = data?.getBooleanExtra("completedTest", false) ?: false
+            val score = data?.getStringExtra("score") ?: ""
+
+            if (completedTest) {
+                // Handle the completion of the questionnaire
+                // Disable the button
+                startButton2.isEnabled = false
+                startButton2.text = "Done"
+                reactionTestResult2.text = score
+            }
         }
     }
 }
