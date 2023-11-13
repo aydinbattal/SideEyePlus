87291d6c49dd30ed08d1661addf30016c0553ee2
Adam Czubernat <czubernatadam@gmail.com>
2023-11-12 21:49:32 -0500

    Implement queue for 30 sec intervals of fatigue interval detection, adding timestamp to queue whenever eyes are closed for 1 sec, then implement while loop to check if top of queue is reach interval time to pop.

--- app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
+++ app/src/main/java/sheridan/czuberad/sideeye/Camera/EyeDetectionUtils.kt
@@ -20,6 +20,8 @@ import sheridan.czuberad.sideeye.Domain.Session
 import sheridan.czuberad.sideeye.EyeDetectionActivity
 import sheridan.czuberad.sideeye.Services.DriverService
 import java.sql.Timestamp
+import java.util.LinkedList
+import java.util.Queue
 import java.util.UUID
 
 class EyeDetectionUtils(
@@ -32,6 +34,7 @@ class EyeDetectionUtils(
     eyeDetectionActivity: EyeDetectionActivity
 ) :ImageAnalyzer<List<Face>>() {
     private var counter = 0
+    private var fatigueCounter = 0
 
     private val realTimeOpts = FaceDetectorOptions.Builder()
         .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
@@ -54,6 +57,10 @@ class EyeDetectionUtils(
     private var alertList = arrayListOf<Alert>()
     private lateinit var sessionUuid:String
     private val contextAct = eyeDetectionActivity
+
+    private val timeQueue: Queue<Long> = LinkedList()
+    private val timeInterval = 30000
+
     override fun detectFace(image: InputImage): Task<List<Face>> {
         return det.process(image)
     }
@@ -86,9 +93,25 @@ class EyeDetectionUtils(
                 isSessionEnd = true
             }
         }
+        var fatigueCounterStartTime = System.currentTimeMillis()
         results.forEach{
+
+            val currentTimeMs = System.currentTimeMillis()
+
+
             if((it.leftEyeOpenProbability!! < 0.5) && (it.rightEyeOpenProbability!! < 0.5)){
                 counter++
+                fatigueCounter++
+
+                if(isSessionStart){
+                    if(fatigueCounter >=15){
+                        timeQueue.add(currentTimeMs)
+                        fatigueCounter = 0
+                        Log.d(TAG, "PPP QUEUE ADD $timeQueue")
+                    }
+                }
+
+                Log.d(TAG, "FatigueCounter: $fatigueCounter")
                 text.text = "EYES NOT DETECTED"
 
                 text.setTextColor(Color.parseColor("#FF0000"))
@@ -96,16 +119,27 @@ class EyeDetectionUtils(
             }
             else if((it.leftEyeOpenProbability!! > 0.5) && (it.rightEyeOpenProbability!! > 0.5)){
                 counter = 0
+                fatigueCounter = 0
                 text.text = "EYES DETECTED"
                 sendMessage(contextAct,"ALERT_INACTIVE", "/SESSION_CURRENT_ALERT")
                 text.setTextColor(Color.parseColor("#00FF0A"))
             }
             else{
                 counter = 0
+                fatigueCounter = 0
                 text.text = "EYES DETECTED"
                 text.setTextColor(Color.parseColor("#00FF0A"))
             }
             if(isSessionStart){
+
+                while (!timeQueue.isEmpty() && currentTimeMs - timeQueue.peek() as Long > timeInterval){
+                    timeQueue.poll()
+                }
+
+                if(timeQueue.size >=5){
+                    Log.d(TAG, "PPP QUEUE REACHED 5")
+                }
+
                 if(counter>=50){
                     eyeLogic = EyeDetectionLogic()
                     sendMessage(contextAct,"ALERT_ACTIVE", "/SESSION_CURRENT_ALERT")
@@ -115,9 +149,11 @@ class EyeDetectionUtils(
                     counter = 0
                     Log.d(TAG, "ALERT:$alertList")
                 }
+
+                Log.d(TAG, "PPP QUEUE END OF LOOP $timeQueue")
+
+
             }
-
-
             Log.d(TAG, "YOO COUNTER: $counter")
             Log.d(TAG,"YOO LEFT EYE" + it.rightEyeOpenProbability.toString())
             Log.d(TAG,"YOO RIGHT EYE" + it.leftEyeOpenProbability.toString())
