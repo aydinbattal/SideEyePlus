a9fc864ce6c6ad64fd37b93efbdffe0839079409
Aydin Battal <abattal35@gmail.com>
2023-11-26 22:09:23 -0500

    Implemented logic for determining if reaction test passed and updated home tests ui

---
+++ app/src/main/java/sheridan/czuberad/sideeye/DeviceUtils.kt
@@ -0,0 +1,27 @@
+package sheridan.czuberad.sideeye
+
+import android.os.Build
+
+/**
+ * SideEye+ created by aydin
+ * student ID : 991521740
+ * on 2023-11-26 */
+
+class DeviceUtils {
+    fun isEmulator(): Boolean {
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.HARDWARE.contains("vbox")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.PRODUCT.contains("sdk")
+                || Build.PRODUCT.contains("google_sdk")
+                || Build.PRODUCT.contains("sdk_google")
+    }
+}
+
